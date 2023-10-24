fe03ecf7bb421a0b95fa91f3d0e9ea7ac1a0f554
Minjun A. Kim <ckandrew04@gmail.com>
2023-10-23 21:41:36 -0400

    Add files via upload

---
+++ Hart ID/Hart ID/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hart ID
+//
+//  Created by Minjun Kim on 3/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hart ID/Hart ID/Hart_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hart_IDApp.swift
+//  Hart ID
+//
+//  Created by Minjun Kim on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Hart_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
